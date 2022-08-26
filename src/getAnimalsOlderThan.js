df66d2aaf87d407d4afc794300a2f02b05b5ac8b
Gabriel Lemes <rlemes.gabriel@gmail.com>
2022-08-26 16:29:58 -0300

    Alterações do lint e remoção dos console.log

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,23 +1,21 @@
 const data = require('../data/zoo_data');
 
-//residents.age >  parametro age
-//data.species.name === animal 
+// residents.age >  parametro age
+// data.species.name === animal
 function getAnimalsOlderThan(animal, age) {
-  const checkingName = data.species.filter((element => element.name === animal))
+  const checkingName = data.species.filter((element) => element.name === animal);
   const checkingAge = checkingName.every((element) => {
-    let result = [];
-    for(let i = 0; i <element.residents.length; i+= 1){
-      if(element.residents[i].age >= age){
+    const result = [];
+    for (let i = 0; i < element.residents.length; i += 1) {
+      if (element.residents[i].age >= age) {
         result.push(element);
       } else {
         return false;
       }
     }
-    return result
-  })
+    return result;
+  });
   return checkingAge;
 }
-// console.log(data.species)
-console.log(getAnimalsOlderThan('otters', 7))
 
 module.exports = getAnimalsOlderThan;
