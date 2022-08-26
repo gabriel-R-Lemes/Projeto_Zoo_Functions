7b41375e309b990e198cb596bb02b01fe9b01613
Gabriel Lemes <rlemes.gabriel@gmail.com>
2022-08-26 16:45:02 -0300

    Realização do Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const result = data.employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
+  return result;
 }
-
 module.exports = getEmployeeByName;
